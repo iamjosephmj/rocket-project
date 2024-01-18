bc1b8a8f28f4228a11932aa77ab1ac234216c371
Joseph James <iamjosephmj@gmail.com>
2024-01-18 08:51:31 +0100

    [added] added final tweaks to the view model.

--- app/src/main/java/com/example/rocketproject/ui/SpaceFlightActivity.kt
+++ app/src/main/java/com/example/rocketproject/ui/SpaceFlightActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.rocketproject.ui.compose.SpaceFlightsScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,9 +25,15 @@ internal class SpaceFlightActivity : ComponentActivity() {
         setContent {
             val viewState by viewModel.viewState.collectAsState()
 
+            LaunchedEffect(key1 = Unit) {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    viewModel.startFlightDataFetch()
+                }
+            }
+
             SpaceFlightsScreen(
                 viewState = viewState,
-                onRetry = viewModel::retry,
+                onRetry = viewModel::startFlightDataFetch,
                 modifier = Modifier.fillMaxSize()
             )
         }
